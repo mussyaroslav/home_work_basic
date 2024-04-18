39dc409f9923809b3059002ad83650aa829d8eb1
Yaroslav <mussyaroslav@gmail.com>
2024-04-18 13:22:01 +0500

    Исправил ошибки

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -1,29 +1,31 @@
-package init
+package main
 
 import (
+	"fmt"
 	"github.com/fixme_my_friend/hw02_fix_app/printer"
 	"github.com/fixme_my_friend/hw02_fix_app/reader"
 	"github.com/fixme_my_friend/hw02_fix_app/types"
-	"fmt"
 )
 
-func init() {
+func main() {
 	var path string = "data.json"
 
-	fmt.Printf("Enter data file path: ")
+	fmt.Printf("Введите путь к файлу данных: ")
 	fmt.Scanln(&path)
+
+	if len(path) == 0 {
+		path = "data.json"
+	}
 
 	var err error
 	var staff []types.Employee
 
-	if len(path) == 0 {
-		path = "data.json"
-	} else {
-	}
-
 	staff, err = reader.ReadJSON(path, -1)
 
-	fmt.Print(err)
+	if err != nil {
+		fmt.Println("Ошибка при чтении данных из файла:", err)
+		return
+	}
 
 	printer.PrintStaff(staff)
 }
