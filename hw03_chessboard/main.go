92458097e3fbbcd774325456a446c756897e8851
mussyaroslav <mussyaroslav@gmail.com>
2024-04-24 13:23:15 +0500

    Сделал дз шахматная доска

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	var size int
+	fmt.Print("Введите размер шахматной доски:")
+	_, err := fmt.Scanln(&size)
+	if err != nil || size < 1 || size == 0 {
+		fmt.Println("Введите число больше нуля!")
+	}
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			if (i+j)%2 == 0 {
+				fmt.Print("#")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println() // Переход на новою строку иначе все будет в 1 линию
+	}
 }
